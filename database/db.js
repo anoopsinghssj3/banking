3680b24b299af6b5dea35cd0e16b331185d7d892
Anoop Singh <anoopsinghssj3@gmail.com>
2025-06-27 13:30:43 +0530

    dv

--- database/db.js
+++ database/db.js
@@ -10,6 +10,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+
         console.log("✅ MongoDB connected successfully...........");
     } catch (err) {
         console.error("❌ MongoDB connection error:", err.message);
