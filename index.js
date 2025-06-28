68153efa23cd8c893332bbb18133e714bfd43a1d
Anoop Singh <anoopsinghssj3@gmail.com>
2025-06-28 08:55:26 +0530

    DF

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
+
+const connectDB = require('./database/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
 
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(require('./router/index'));
 
-const connectDB = require('./database/db.js');
-connectDB();
-
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
